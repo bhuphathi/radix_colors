115d83c249fae84adcc32619ba6a8b7ec91c63e1
bhuphathi <bhuphathi@gmail.com>
2023-09-25 16:50:49 +0530

    feat: documentation template

--- lib/src/radix_color.dart
+++ lib/src/radix_color.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/painting.dart';
 
 class RadixColor extends ColorSwatch<int> {
+  /// {@template RadixColor}
   /// ### Radix Colors provides 15 color scales, designed for white foreground text at step 9
   /// There are 12 steps in each scale. Each step was designed for at least one specific use case.
   ///
@@ -27,8 +28,10 @@ class RadixColor extends ColorSwatch<int> {
   /// 11 = Low-contrast text
   ///
   /// 12 = High-contrast text
+  /// {@endtemplate}
   const RadixColor(super.primary, super.swatch, {this.name});
 
+  /// {@template RadixColor.step1}
   /// For app backgrounds and subtle component backgrounds.
   ///
   /// Appropriate applications include:
@@ -46,8 +49,10 @@ class RadixColor extends ColorSwatch<int> {
   /// * Canvas area background
   ///
   /// You may want to use white for your app background in light mode, and Step 1 or 2 from a gray or coloured scale in dark mode.
+  /// {@endtemplate}
   Color get step1 => this[1]!;
 
+  /// {@template RadixColor.step2}
   /// For app backgrounds and subtle component backgrounds.
   ///
   /// Appropriate applications include:
@@ -65,8 +70,10 @@ class RadixColor extends ColorSwatch<int> {
   /// * Canvas area background
   ///
   /// You may want to use white for your app background in light mode, and Step 1 or 2 from a gray or coloured scale in dark mode.
+  /// {@endtemplate}
   Color get step2 => this[2]!;
 
+  /// {@template RadixColor.step3}
   /// For UI component backgrounds.
   ///
   /// Step 3 is for normal states.
@@ -84,8 +91,10 @@ class RadixColor extends ColorSwatch<int> {
   /// * Step 6 for hovered "unread" backgrounds.
   ///
   /// Steps 11 and 12—which are designed for text—are guaranteed to pass 4.5:1 contrast ratio on top of a step 3 background from the same scale.
+  /// {@endtemplate}
   Color get step3 => this[3]!;
 
+  /// {@template RadixColor.step4}
   /// For UI component backgrounds.
   ///
   /// Step 4 is for hover states.
@@ -103,8 +112,10 @@ class RadixColor extends ColorSwatch<int> {
   /// * Step 6 for hovered "unread" backgrounds.
   ///
   /// Steps 11 and 12—which are designed for text—are guaranteed to pass 4.5:1 contrast ratio on top of a step 3 background from the same scale.
+  /// {@endtemplate}
   Color get step4 => this[4]!;
 
+  /// {@template RadixColor.step5}
   /// For UI component backgrounds.
   ///
   /// Step 5 is for pressed or selected states
@@ -122,17 +133,25 @@ class RadixColor extends ColorSwatch<int> {
   /// * Step 6 for hovered "unread" backgrounds.
   ///
   /// Steps 11 and 12—which are designed for text—are guaranteed to pass 4.5:1 contrast ratio on top of a step 3 background from the same scale.
+  /// {@endtemplate}
   Color get step5 => this[5]!;
 
+  /// {@template RadixColor.step6}
   /// Step 6 is designed for subtle borders on components which are not interactive. For example sidebars, headers, cards, alerts, and separators.
+  /// {@endtemplate}
   Color get step6 => this[6]!;
 
+  /// {@template RadixColor.step7}
   /// Step 7 is designed for borders on interactive components, but can also be used for focus rings.
+  /// {@endtemplate}
   Color get step7 => this[7]!;
 
+  /// {@template RadixColor.step8}
   /// Step 8 is designed for borders on interactive components in their hover state.
+  /// {@endtemplate}
   Color get step8 => this[8]!;
 
+  /// {@template RadixColor.step9}
   /// Designed for solid backgrounds.
   ///
   /// Step 9 has the highest chroma of all steps in the scale. In other words, it's the purest step, the step mixed with the least amount of white or black. Because 9 is the purest step, it has a wide range of applications:
@@ -154,15 +173,22 @@ class RadixColor extends ColorSwatch<int> {
   /// * Accent borders
   ///
   /// Most step 9 colors are designed for white foreground text. Sky, Mint, Lime, Yellow, and Amber are designed for black foreground text and steps 9 and 10.
+  /// {@endtemplate}
   Color get step9 => this[9]!;
 
+  /// {@template RadixColor.step10}
   /// Step 10 is designed for component hover states, where step 9 is the component's normal state background.
+  /// {@endtemplate}
   Color get step10 => this[10]!;
 
+  /// {@template RadixColor.step11}
   /// Step 11 is designed for low-contrast text.
+  /// {@endtemplate}
   Color get step11 => this[11]!;
 
+  /// {@template RadixColor.step12}
   /// Step 12 is designed for high-contrast text.
+  /// {@endtemplate}
   Color get step12 => this[12]!;
 
   /// Name of color scale
